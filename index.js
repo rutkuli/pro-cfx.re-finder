0d5b75daeeefdce5303c51a245c4aab6574480d4
ImFrosher <32975828+ImFrosher@users.noreply.github.com>
2021-02-23 10:01:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+var request = require('request')
+var isUrl = require("is-url")
+
+
+function requestUrl(url) {
+    return new Promise((resolve, reject) => {
+        request({
+            method: 'GET',
+            uri: url,
+            "rejectUnauthorized": false,
+        }, function(error, response, body) {
+            // body is the decompressed response body
+            if (error) {
+                reject("ERROR");
+            }
+            resolve(response.headers["x-citizenfx-url"])
+        })
+    });
+}
+
+
+async function tryRequest() {
+    if (isUrl(process.argv[2])) {
+        requestUrl(process.argv[2]).then(asd => {
+            if (!isNaN(asd.charAt(0))) {
+                console.log("FOUND: " + asd);
+            } else {
+                console.log("Couldn't find retrying...")
+                tryRequest();
+            }
+        })
+    } else {
+        console.log("Argument is not an Url!\nUsage: node filename.js <link to cfx.re>");
+        process.exit(69);
+    }
+}
+
+tryRequest();
